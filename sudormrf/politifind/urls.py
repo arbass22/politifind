f3c3ad98fdb0124f49b2e6ea9603d6cb3ae1d059
Justin Kennedy <jekennedy@umass.edu>
2017-10-25 00:16:35 -0400

    added bill page and changed view structure

--- sudormrf/politifind/urls.py
+++ sudormrf/politifind/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^politician/(.*)', views.politician, name='politician'),
+    url(r'^bill/(.*)', views.bill, name='bill'),
 ]
---
+++ sudormrf/politifind/views/__init__.py
@@ -0,0 +1,3 @@
+from .index import index
+from .bill import bill
+from .politician import politician
