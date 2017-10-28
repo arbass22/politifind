7c76e81cf687ec6b71e4d45dcc16ce7a9bbddd27
Matt Gramigna <mgramigna@umass.edu>
2017-10-28 14:53:52 -0400

    Added sponsor as foreign key directly in bills table

--- sudormrf/politifind/scripts/bills.py
+++ sudormrf/politifind/scripts/bills.py
@@ -1,5 +1,5 @@
 import requests
-from politifind.models import Bill
+from politifind.models import Bill, Politician
 
 def run():
     api_header = { 'X-API-KEY': 'DB9r6gpgLzulQ9Z2GQKbWfLWXh9FtrK9utwjoFZk' }
@@ -7,6 +7,9 @@ def run():
     senate = requests.get('https://api.propublica.org/congress/v1/115/senate/bills/introduced.json', headers=api_header).json().get('results')[0].get('bills')
     bills = house + senate
     for bill in bills:
-        new_bill = Bill(bid=bill.get('bill_id'), code=bill.get('number'), name=bill.get('title'), status=bill.get('senate_passage') or bill.get('house_package'), subject=bill.get('primary_subject'), summary=bill.get('summary_short'), latest_action_date=bill.get('latest_major_action_date'), latest_action=bill.get("latest_major_action"))
-        new_bill.save()
+        try:
+            new_bill = Bill(bid=bill.get('bill_id'), code=bill.get('number'), name=bill.get('title'), status=bill.get('senate_passage') or bill.get('house_package'), subject=bill.get('primary_subject'), summary=bill.get('summary_short'), latest_action_date=bill.get('latest_major_action_date'), latest_action=bill.get("latest_major_action"), sponsor_pid=Politician.objects.get(pid=bill.get('sponsor_id')))
+            new_bill.save()
+        except Exception:
+            pass
 
