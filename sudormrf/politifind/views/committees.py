b73170c77f347c1f7afe717603c901977c501a93
Andrew Bass <arbass@umass.edu>
2017-10-31 13:04:33 -0400

    Added basic committee structure

--- sudormrf/politifind/views/committees.py
+++ sudormrf/politifind/views/committees.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 def committees(request):
+    committee = {
+        "name": "Armed Forces Committee",
+        "description": "The Senate Committee on Armed Services has legislative jurisdiction over military and defense",
+        "makeup": {
+            "total": 22,
+            "republican": 10,
+            "democrat": 10,
+            "independent": 2,
+        },
+    }
+
     return render(
         request,
         'committees.html',
-        context={},
+        context={"committee":committee},
     )
