781ae3992e7b4a74f54a9e41291ea21263b7b07e
Matt Biz <mpbiz1158@gmail.com>
2017-10-31 14:14:30 -0400

    Committee Membership Script

---
+++ sudormrf/politifind/scripts/committeemembership.py
@@ -0,0 +1,12 @@
+from random import randrange
+from politifind.models import Politician, Committee, CommitteeMembership
+
+def run():
+    r = "member"
+    politicians = Politician.objects.all()
+    committees = Committee.objects.all()
+    for p in politicians:
+        i = randrange(0, 6)
+        for i in range(0, i):
+            random_index = randrange(0, len(committees))
+            cm = CommitteeMembership(cid=Committee.objects.get(cid=committees[random_index].cid), pid=Politician.objects.get(pid=p.pid), relationship=r)
