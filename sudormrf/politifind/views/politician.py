b0fba80d00b5cf16d333b958eb420ee5b7d73798
Andrew Bass <arbass@umass.edu>
2017-11-01 02:21:53 -0400

    Added politician dynamic tabs

--- sudormrf/politifind/views/politician.py
+++ sudormrf/politifind/views/politician.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 from politifind.models import Politician, PoliticianVote, CommitteeMembership, BillSponsorship
 
 def politician(request, pid, page=None):
@@ -7,26 +8,48 @@ def politician(request, pid, page=None):
     committee_membership = CommitteeMembership.objects.filter(pid__exact=pid)
     bill_sponsorship = BillSponsorship.objects.filter(pid__exact=pid)
 
+    page_context = {
+        "tabs": [{
+            "name": "Home",
+            "url": reverse('politician', args=[politician.pid]),
+            },
+            {
+            "name": "Votes",
+            "url": reverse('politician', args=[politician.pid, "votes"]),
+            },
+            {
+            "name": "Bills",
+            "url": reverse('politician', args=[politician.pid, "bills"]),
+            }],
+    }
+
     context = {
         'politician': politician,
         'committee_membership': committee_membership,
         'bill_sponsorship': bill_sponsorship,
         'recent_votes': recent_votes,
+        'page': page_context,
     }
 
+
     if page == 'votes':
+        print "Hello"
+        context['page']['active_tab'] = "Votes"
         return render(
             request,
             'politician_votes.html',
             context=context,
         )
     elif page == 'bills':
+        context['page']['active_tab'] = "Bills"
         return render(
             request,
             'politician_bills.html',
             context=context,
         )
     else:
+        print "Hello2"
+        context['page']['active_tab'] = "Home"
         return render(
             request,
             'politician_home.html',
