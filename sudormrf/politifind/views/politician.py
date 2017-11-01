76e0ee10ef0fc329bb957e4928564ccc328e3f7b
Matt Gramigna <mgramigna@umass.edu>
2017-10-31 21:17:07 -0400

    Cleaned up politician pages with real data

--- sudormrf/politifind/views/politician.py
+++ sudormrf/politifind/views/politician.py
@@ -1,72 +1,14 @@
 from django.shortcuts import render
-from politifind.models import Politician
+from politifind.models import Politician, PoliticianVote, CommitteeMembership, BillSponsorship
 
 def politician(request, pid, page=None):
-    pol = Politician.objects.get(pid=pid)
-    # politician = {
-    #     "name": "Bernie Sanders",
-    #     "position": "Senator",
-    #     "state": "Vermont",
-    #     "image_url": "https://goo.gl/ZSzSK1",
-    #     "party": "I",
-    #     "facebook_handle": "@bernie",
-    #     "twitter_hanlde": "@bernie",
-    #     "youtube_handle": "@bernie",
-    # }
-
-    committee_membership = [
-        {
-            "name": "Committee on the Budget",
-            "tenure": 3
-        },
-        {
-            "name": "Other Committee",
-            "tenure": 5
-        },
-        {
-            "name": "Socialist Committee",
-            "tenure": 10
-        }
-    ]
-
-    bill_sponsorship = [
-        {
-            "name": "Medicare for All Act of 2017",
-            "code": "S.1804",
-            "status": "In committee",
-        },
-        {
-            "name": "Othe bill that does something",
-            "code": "S.1804",
-            "status": "In committee"
-        },
-        {
-            "name": "Here is a bill that has a long name since that is something that a lot of bills have",
-            "code": "S.1804",
-            "status": "Passed in House"
-        },
-    ]
-
-    recent_votes = [
-        {
-            "name": "Medicare for All Act of 2017",
-            "code": "S.1804",
-            "vote": "Yay",
-        },
-        {
-            "name": "Othe bill that does something",
-            "code": "S.1804",
-            "vote": "Nay",
-        },
-        {
-            "name": "Here is a bill that has a long name since that is something that a lot of bills have",
-            "code": "HR.123",
-            "vote": "Yay",
-        },
-    ]
+    politician = Politician.objects.get(pid=pid)
+    recent_votes = PoliticianVote.objects.filter(pid__exact=pid)
+    committee_membership = CommitteeMembership.objects.filter(pid__exact=pid)
+    bill_sponsorship = BillSponsorship.objects.filter(pid__exact=pid)
 
     context = {
-        'politician': pol,
+        'politician': politician,
         'committee_membership': committee_membership,
         'bill_sponsorship': bill_sponsorship,
         'recent_votes': recent_votes,
