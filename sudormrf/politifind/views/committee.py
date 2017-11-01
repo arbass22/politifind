960127b185083c6e2aeab07a3834068f22f8d267
Andrew Bass <arbass@umass.edu>
2017-11-01 14:32:56 -0400

    Added committee bills routing

--- sudormrf/politifind/views/committee.py
+++ sudormrf/politifind/views/committee.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
+from django.urls import reverse
 from politifind.models import Committee
 
-def committee(request, cid):
+def committee(request, cid, page=None):
     committee = Committee.objects.get(cid=cid)
-    page = {
+    page_context = {
         "tabs": [{
             "name": "Members",
-            "url": "#",
+            "url": reverse('committee', args=[committee.cid]),
             },
             {
             "name": "Bills",
-            "url": "#",
+            "url": reverse('committee', args=[committee.cid, "bills"]),
             }],
         "active_tab": "Members"
     }
 
-    return render(
-        request,
-        'committees_members.html',
-        context={"committee":committee, "page":page},
-    )
+    if page == "members":
+        page_context["active_tab"] = "Members"
+        return render(
+            request,
+            'committees_members.html',
+            context={"committee":committee, "page":page_context},
+        )
+    elif page == "bills":
+        page_context["active_tab"] = "Bills"
+        return render(
+            request,
+            'committees_bills.html',
+            context={"committee":committee, "page":page_context},
+        )
+    else:
+        return render(
+            request,
+            'committees_members.html',
+            context={"committee":committee, "page":page_context},
+        )
