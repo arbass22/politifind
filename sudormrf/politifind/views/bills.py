278a30d11766a8e497de2e24261a36708f9573e3
Matt Gramigna <mgramigna@umass.edu>
2017-11-01 11:46:40 -0400

    Added list of bills to main bills route

--- sudormrf/politifind/views/bills.py
+++ sudormrf/politifind/views/bills.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from politifind.models import Bill
 
 def bills(request):
+    bills = Bill.objects.all()
+
     return render(
         request,
         'bills.html',
-        context={},
+        context={'bills': bills},
     )
