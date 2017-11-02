a8a526bcc1625ad6250483f78cf578bb1b03d462
Matt Gramigna <mgramigna@umass.edu>
2017-11-02 12:25:53 -0400

    Fixed subscriptions in profile page

--- sudormrf/politifind/views/profile.py
+++ sudormrf/politifind/views/profile.py
@@ -1,13 +1,16 @@
 from random import randrange
+from itertools import chain
 from django.shortcuts import render
-from politifind.models import User
+from politifind.models import User, UserBillSubscription, UserCommitteeSubscription, UserPoliticianSubscription
 
 def profile(request):
     users = User.objects.all()
     user = users[randrange(0,len(users))]
-
+    bill_subscriptions = UserBillSubscription.objects.filter(uid__exact=user)
+    committee_subscriptions = UserCommitteeSubscription.objects.filter(uid__exact=user)
+    politician_subscriptions = UserPoliticianSubscription.objects.filter(uid__exact=user)
     return render(
         request,
         'profile.html',
-        context={ 'user':user },
+        context={ 'user':user, 'bill_subscriptions': bill_subscriptions, 'committee_subscriptions': committee_subscriptions, 'politician_subscriptions': politician_subscriptions },
     )
