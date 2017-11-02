c935a6d7b8060a8ef8061fbc1f935fdcf654fd5c
Matt Gramigna <mgramigna@umass.edu>
2017-11-02 12:25:32 -0400

    Fixed user info script

--- sudormrf/politifind/scripts/userinfo.py
+++ sudormrf/politifind/scripts/userinfo.py
@@ -1,7 +1,7 @@
 import random
 import math
 from random import randrange
-from politifind.models import User, UserVote, Bill, UserPoliticianSubscription, Politician, UserBillSubscription, Committee
+from politifind.models import User, UserVote, Bill, UserPoliticianSubscription, Politician, UserBillSubscription, Committee, UserCommitteeSubscription
 
 def rand(maximum, minimum):
     return str(int(minimum + math.floor(random.random()*(maximum-minimum))))
@@ -19,11 +19,11 @@ def run():
     user3 = User(uid=3, name="Sara Autumne", username="sautumne", email="sautumn@email.com", password="compsci326", party="I", picture="http://placehold.it/150x150")
     user4 = User(uid=4, name="Veronika Hils", username="vhils", email="vhils@email.com", password="compsci326", party="R", picture="http://placehold.it/150x150")
     
-    #user0.save()
-    #user1.save()
-    #user2.save()
-    #user3.save()
-    #user4.save()
+    user0.save()
+    user1.save()
+    user2.save()
+    user3.save()
+    user4.save()
     
     users = User.objects.all()
     bills = Bill.objects.all()
@@ -40,26 +40,26 @@ def run():
             else:
                 v = "NAY"
             r_index1 = randrange(0, len(bills))
-            uv = UserVote(uid=User.objects.get(uid=u.uid).uid, bid=Bill.objects.get(bid=bills[r_index1].bid).bid, vote=v, date_voted=getADate())
-            #uv.save()
+            uv = UserVote(uid=User.objects.get(uid=u.uid), bid=Bill.objects.get(bid=bills[r_index1].bid), vote=v, date_voted=getADate())
+            uv.save()
         y = randrange(1,5)
         i = 0
         while(i < y):
             i = i + 1
             r_index2 = randrange(0, len(ps))
-            ups = UserPoliticianSubscription(uid=User.objects.get(uid=u.uid).uid, pid=Politician.objects.get(pid=ps[r_index2].pid).pid, date_subscribed=getADate())
-            #ups.save()
+            ups = UserPoliticianSubscription(uid=User.objects.get(uid=u.uid), pid=Politician.objects.get(pid=ps[r_index2].pid), date_subscribed=getADate())
+            ups.save()
         i = 0
         w = randrange(1,5)
         while(i < w):
             i = i + 1
             r_index3 = randrange(0, len(bills))
-            ubs = UserBillSubscription(uid=User.objects.get(uid=u.uid).uid, bid=Bill.objects.get(bid=bills[r_index3].bid).bid, date_subscribed=getADate())
-            #ubs.save()
+            ubs = UserBillSubscription(uid=User.objects.get(uid=u.uid), bid=Bill.objects.get(bid=bills[r_index3].bid), date_subscribed=getADate())
+            ubs.save()
         i = 0
         k = randrange(1, 5)
         while(i < k):
             i = i + 1
             r_index4 = randrange(0, len(cs))
-            ucs = UserCommitteeSubscription(uid=User.objects.get(uid=u.uid).uid, cid=Committee.objects.get(cid=cs[r_index4].cid).cid, date_subscribed=getADate())
-            #ucs.save()
+            ucs = UserCommitteeSubscription(uid=User.objects.get(uid=u.uid), cid=Committee.objects.get(cid=cs[r_index4].cid), date_subscribed=getADate())
+            ucs.save()
