f1446a63d3e51a355e2c1547faff828b106da993
Matt Gramigna <mgramigna@umass.edu>
2017-11-25 13:38:49 -0500

    added pagination to politicians

--- sudormrf/politifind/views/politicians.py
+++ sudormrf/politifind/views/politicians.py
@@ -1,8 +1,18 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from politifind.models import Politician
 
 def politicians(request):
-    politicians = Politician.objects.all()
+    politicians_list = Politician.objects.all()
+    paginator = Paginator(politicians_list, 25)
+
+    page = request.GET.get('page')
+    try:
+        politicians = paginator.page(page)
+    except PageNotAnInteger:
+        politicians = paginator.page(1)
+    except EmptyPage:
+        politicians = paginator.page(paginator.num_pages)
 
     return render(
         request,
