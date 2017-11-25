3af70ea2b22f6aa17c2a60b821d7dd1cbc6c4957
Matt Gramigna <mgramigna@umass.edu>
2017-11-24 22:23:51 -0500

    encoded bill names

--- sudormrf/politifind/views/bills.py
+++ sudormrf/politifind/views/bills.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from politifind.models import Bill
+import unidecode
 
 def bills(request):
     bills = Bill.objects.all()
+    for b in bills:
+        b.name = unidecode.unidecode(b.name)
 
     return render(
         request,
