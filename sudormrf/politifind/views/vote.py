9a16f4fbdcd6c8bb3bf516051cd76350f1587eaf
Justin Kennedy <jekennedy@umass.edu>
2017-11-27 21:43:43 -0500

    finished write up and stored comment in votes

--- sudormrf/politifind/views/vote.py
+++ sudormrf/politifind/views/vote.py
@@ -6,6 +6,6 @@ def vote(request, bid, user_vote):
     comment = request.POST.get('comment', None)
     bill = Bill.objects.get(bid=bid)
     profile = Profile.objects.get(user=request.user)
-    new_user_vote = UserVote(user=profile, bill=bill, vote=user_vote, date_voted=datetime.now().date())
+    new_user_vote = UserVote(user=profile, bill=bill, vote=user_vote, date_voted=datetime.now().date(), comment=comment)
     new_user_vote.save()
     return redirect('bill', bid)
